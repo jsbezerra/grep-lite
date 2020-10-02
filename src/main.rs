38233f0d758ae20c0f20b2c1a8836276df7e61ef
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-02 12:53:56 -0300

    feat: include regex

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use regex::Regex;
+
 fn main() {
-    let search_term = "picture";
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
+    let regex = Regex::new("picture").unwrap();
+
+    let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
 
     for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line);
+        let contains_substring = regex.find(line);
+        match contains_substring {
+            Some(_) => println!("{}: {}", i+1, line),
+            None => (),
         }
     }
 }
